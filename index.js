19a8f270a9e72d0530ed84e5764b504dbc6a90db
Tech5818 <pqow5818@gmail.com>
2024-08-14 09:26:11 +0900

    Feat: Add move player&enemy

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ class Sprite {
 
     this.width = 50;
     this.height = 150;
+
+    this.lastkey;
   }
 
   draw() {
@@ -28,6 +30,8 @@ class Sprite {
     this.draw();
 
     this.position.y += this.velocity.y;
+
+    this.position.x += this.velocity.x;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
       this.velocity = {
@@ -51,6 +55,12 @@ const player = new Sprite({
   },
 });
 
+const speed = {
+  left: -5,
+  right: 5,
+  jump: -10
+}
+
 const enemy = new Sprite({
   position: {
     x: 400,
@@ -62,6 +72,27 @@ const enemy = new Sprite({
   },
 });
 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowUp: {
+    pressed: false
+  },
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
 
@@ -70,6 +101,77 @@ function animate() {
 
   player.update();
   enemy.update();
+
+  // 가만히 있었을때 움직이지 않게하기
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+
+  if (keys.a.pressed && player.lastkey === "a") {
+    player.velocity.x = speed.left;
+  } else if (keys.d.pressed && player.lastkey === "d") {
+    player.velocity.x = speed.right
+  }
+
+  if (keys.ArrowLeft.pressed && enemy.lastkey === "ArrowLeft") {
+    enemy.velocity.x = speed.left;
+  } else if (keys.ArrowRight.pressed && enemy.lastkey === "ArrowRight") {
+    enemy.velocity.x = speed.right;
+  }
 }
 
 animate();
+
+// 키보드를 눌렀을 때, 이벤트 발생
+window.addEventListener("keydown", e => {
+  console.log(e.key);
+
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = true;
+      player.lastkey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player.lastkey = "a";
+      break;
+    case "w":
+      player.velocity.y = -10
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      enemy.lastkey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      enemy.lastkey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      enemy.velocity.y = -10
+      break;
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", e => {
+  switch (e.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "ArrowUp":
+      break;
+    default:
+      break;
+  }
+})
